7c652c793dd6cf65c5b0f4ca108a8c5f68e8e880
blasut <blasut937@gmail.com>
2018-06-14 19:23:06 +0200

    fix

--- matrix.js
+++ matrix.js
@@ -5,16 +5,37 @@ function checkEq(m1, m2) {
   return eq;
 }
 // https://www.varsitytutors.com/hotmath/hotmath_help/topics/scalar-multiplication-of-matrices.html
+var matrix = [
+  [{x: 0, y: 0},
+   {x: 0, y: 1},
+   {x: 0, y: 2}],
+  [{x: 1, y: 0},
+   {x: 1, y: 1},
+   {x: 1, y: 2}],
+  [{x: 2, y: 0},
+   {x: 2, y: 1},
+   {x: 2, y: 2}],
+];
+
+var lens = (getter, setter) => {
+  return ({
+    get: obj => getter(obj),
+    set: (val, obj) => setter(val, obj),
+  })
+}
+
+
+
 function dilateMatrix(cols, rows, matrix, scaleFactor) {
   var new_matrix = Array(cols);
-  for (var i = 0; i < cols; i++) {
-    new_matrix[i] = Array(rows);
-    for (var y = 0; y < rows; y++) {
-      console.log("find matrix:", i,y);
-      console.log("calc new x:", matrix[i][y].x, "*", scaleFactor);
-      console.log("calc new y:", matrix[i][y].y, "*", scaleFactor);
-      new_matrix[i][y] = {x: matrix[i][y].x * scaleFactor,
-                          y: matrix[i][y].y * scaleFactor};
+  for (var c = 0; c < cols; c++) {
+    new_matrix[c] = Array(rows);
+    for (var r = 0; r < rows; r++) {
+      console.log("find matrix:", c,r);
+      console.log("calc new x:", matrix[c][r].x, "*", scaleFactor);
+      console.log("calc new r:", matrix[c][r].y, "*", scaleFactor);
+      new_matrix[c][r] = {x: matrix[c][r].x * scaleFactor,
+                          y: matrix[c][r].y * scaleFactor};
     }
   }
 
